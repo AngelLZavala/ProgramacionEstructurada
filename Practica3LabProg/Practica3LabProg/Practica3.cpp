0287e737be0db218f4579df2cc03089310472213
AngelLZavala <60493480+AngelLZavala@users.noreply.github.com>
2020-02-13 21:22:29 -0600

    Add files via upload
    Actualice la practica 3 me había faltado la comparación con símbolos

---
+++ Practica3LabProg/Practica3LabProg/Practica3.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	char x;
+	printf("Escriba un caracter:");
+	x=getchar();
+	int b= (int) x;
+	if(b>=48 && b<=57){
+	   printf("\n Es numerico.");
+    }else{
+    if(b>=65 && b<=90){
+        printf("\n Es alfabetico mayuscula:");
+    }else{
+    if(b>=97 && b<=122){
+    	printf("\n Es alfabetico minuscula:");
+	}else{
+		printf("\n Es un simbolo:");
+	    }
+	  }
+   }
+   return 0;
+}
